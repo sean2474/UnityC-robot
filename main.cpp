b33cd09879f0712d441edcda085132a77a53a973
sean2474 <47488868+sean2474@users.noreply.github.com>
2020-07-12 11:51:42 +0900

    printOnetoTen
    added function printing one to ten

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ void printNumber() {
 	std::cout << "1" << std::endl;
 }
 
+void printOnetoTen(){
+	for(int i = 0; i < 10; i++) {
+		std::cout << i << std::endl;
+	}
+}
+
 int main(){
 	std::cout << "hello world!!" << std::endl;
 	std::cout << "This is Kyo and I also want to say hello world" << std::endl;

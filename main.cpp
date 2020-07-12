ad41c91f892948681686a263560200f18c264ec5
kyeoreheo <91kyoheo@gmail.com>
2020-07-11 22:39:32 -0400

    print number 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+void printNumber() {
+	std::cout << "1" << std::endl;
+}
+
 int main(){
 	std::cout << "hello world!!" << std::endl;
 	std::cout << "This is Kyo and I also want to say hello world" << std::endl;

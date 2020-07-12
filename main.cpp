763ed957ad3a2fef7e1dce973fb08179905dc23d
kyeoreheo <91kyoheo@gmail.com>
2020-07-11 22:37:01 -0400

    Very first commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(){
-	std::cout << "hello world" << std::endl;
+	std::cout << "hello world!!" << std::endl;
+	std::cout << "This is Kyo and I also want to say hello world" << std::endl;
 	return 0;
 }
